8a01a67a0cbdaa4a73c647e74acc82beb0bd6e08
Renato Lins <renato.lins.digital@outlook.com>
2024-09-06 16:24:55 -0300

    updates the Title component

--- src/shared/components/Title/Title.model.ts
+++ src/shared/components/Title/Title.model.ts
@@ -1,4 +1,5 @@
 export interface TitleProps {
   text: string;
+  className?: string;
   tag?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 }
--- src/shared/components/Title/Title.tsx
+++ src/shared/components/Title/Title.tsx
@@ -1,8 +1,11 @@
 import "./Title.scss";
+import classNames from "classnames";
 import { TitleProps } from "./Title.model";
 
-function Title({ text, tag: Tag = "h1" }: TitleProps) {
-  return <Tag className={`title heading-${Tag}`}>{text}</Tag>;
+function Title({ text, tag: Tag = "h1", className = "" }: TitleProps) {
+  const classes = classNames("title", className, Tag);
+
+  return <Tag className={classes}>{text}</Tag>;
 }
 
 export default Title;
