fde89010811041ab7a1138c477052a7703a9993f
Renato Lins <renato.lins.digital@outlook.com>
2024-09-05 21:13:38 -0300

    updates vite config

--- src/domain/components/AppHeader/AppHeader.tsx
+++ src/domain/components/AppHeader/AppHeader.tsx
@@ -1,12 +1,12 @@
 import "./styles.scss";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "@/domain/images/react.svg";
 
 function AppHeader() {
   return (
     <div className="app-header-container">
       <a href="./" className="logo-and-title-container">
         <img src={reactLogo} className="logo" alt="App logo" />
-        <span>To do App</span>
+        <span>Boilerplate App</span>
       </a>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -26,9 +26,9 @@ module.exports = {
       },
       fontFamily: {
         text: ['"TitilliumWeb Regular"', "sans-serif"], // Body text
-        textBold: ['"TitilliumWeb SemiBold"', "sans-serif"], // Bold text
+        textBold: ['"TitilliumWeb SemiBold"', "sans-serif"], // Bold/Emphasis text
         heading: ['"TitilliumWeb Bold"', "sans-serif"], // Main headings (h1)
-        subheading: ['"TitilliumWeb SemiBold"', "sans-serif"], // Subheadings (h2)
+        subheading: ['"TitilliumWeb SemiBold"', "sans-serif"], // Subheadings (h2, h3, etc.)
         linkText: ['"TitilliumWeb SemiBold"', "sans-serif"], // Links
         ui: ['"TitilliumWeb SemiBold"', "sans-serif"], // UI elements like buttons, inputs
       },
@@ -36,3 +36,82 @@ module.exports = {
   },
   plugins: [],
 };
+
+/*
+
+const defaultTheme = {
+  // Light theme as default
+  isInDarkMode: false,
+  colors: {
+    ...neutralColors,
+    ...semanticColors,
+    ...visualIdentityColors
+  },
+
+  fontFamilies: {
+    body: ['"TitilliumWeb Regular"', 'sans-serif'],
+    bodyBold: ['"TitilliumWeb SemiBold"', 'sans-serif'],
+    heading1: ['"TitilliumWeb Bold"', 'sans-serif'],
+    heading2: ['"TitilliumWeb SemiBold"', 'sans-serif'],
+    link: ['"TitilliumWeb SemiBold"', 'sans-serif'],
+    uiElements: ['"TitilliumWeb SemiBold"', 'sans-serif']
+  },
+  fontSizesPx: {
+    smallest: 12,
+    small: 14,
+    default: 16,
+    large: 18,
+    extraLarge: 22,
+    huge: 24,
+    jumbo: 32,
+    biggest: 40
+  },
+  fontSizes: {
+    smallest: 0.75,
+    small: 0.875,
+    default: 1,
+    large: 1.125,
+    extraLarge: 1.375,
+    huge: 1.5,
+    jumbo: 2,
+    biggest: 2.5
+  },
+
+  fontWeights: {
+    light: 300,
+    regular: 400,
+    medium: 500,
+    bold: 700
+  },
+
+  lineHeights: {
+    small: 1,
+    default: 1.2,
+    tall: 1.5,
+    taller: 1.8,
+    big: 2,
+    biggest: 2.4
+  },
+
+  // Timings 
+
+  transitions: {
+    defaultAll: 'all .2s ease',
+    defaultTime: '.2s'
+  },
+
+  variantColors: {
+    // Light theme colors as default
+    text: '#221e22',
+    heading: '#111111',
+    textBold: '#111111',
+    buttonText: '#fafafa',
+    appBackground: '#fafafa',
+    buttonBackground: '#221e22',
+    containerBackground: '#dfdfdf'
+  }
+};
+
+export default defaultTheme;
+
+*/
