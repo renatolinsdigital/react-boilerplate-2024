27e5110ec7937e8e9fa0d39d1003a649922c3a9d
Renato Lins <renato.lins.digital@outlook.com>
2024-09-06 22:06:04 -0300

    updates layout structure

--- src/domain/pages/Home.tsx
+++ src/domain/pages/Home.tsx
@@ -5,9 +5,9 @@ function Home() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="page-container">
+    <div className="page-container p-5">
       <Title>To Do App</Title>
-      <div className="content-container">
+      <div className="content-container column gap-2">
         <Button onClick={() => setCount((count) => count + 1)}>Click me</Button>
         <Text>Count is {count}</Text>
       </div>
